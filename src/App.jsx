145037991af738e92a67c4af3bcca707b0a6dc30
anujgarg28 <91684786+anujgarg28@users.noreply.github.com>
2021-10-01 13:00:43 +0530

    Updating App.jsx - 30th September 2021

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
 import React from 'react'
+import Data from './Data.jsx'
 
-const arr = [ { id: 10, name: 'Anuj'}, {id: 20, name: 'Rahul'}, {id: 30, name:'Mohit'}]
-const heading = "Test React";
+//const arr = [ { id: 10, name: 'Anuj'}, {id: 20, name: 'Rahul'}, {id: 30, name:'Mohit'}]
+//const heading = "Test React";
 
 export default class App extends React.Component {
     constructor() {
         super();
-        //this.getData = this.getData.bind(this);
+        this.state = {
+            heading: 'Test React',
+            name: '',
+            arr: [ { id: 10, name: 'Anuj'}, {id: 20, name: 'Rahul'}, {id: 30, name:'Mohit'}]
+        }
     }
 
     getData = () => {
-        console.log("Button Clicked");
-        console.log(this); 
+        const oldArr = this.state.arr;
+        oldArr.push({id: 40, name: 'Sumit'});
+        this.setState({
+           heading: 'My Heading is Changed',
+           arr: oldArr,
+           name: 'Anuj'
+        });
     }
 
     render() {
         return (
             <React.Fragment>
-                <h1> { heading } </h1>
-               { arr.map((element) => <h4> {element.id} , {element.name }</h4> )}
-                <button onClick={this.getData}>Click Me</button>
+                <h1> { this.state.heading } </h1>
+               {this.state.arr.map((element) => <h4> {element.id} , {element.name }</h4> )}
+               <button onClick={this.getData} disabled={this.state.name}>Click Me</button>
+               {this.state.name && 
+               <Data 
+                    name={this.state.name} 
+                    city = {'Gurgaon'}
+                    phone = {2832832032}
+               />}
             </React.Fragment>
         )
     }
